787587d62934db653135c13bbdda1d38ea2c38e7
bivol <vbivol394@gmail.com>
2026-09-17 11:11:27 +0300

    first commit

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    class PostType(models.TextChoices):
+        VIDEO = "Vid", "Video"
+        SOUND = "Music", "Sound"
+        IMAGE = "Gif", "Gifs"
+        FREE = "Free", "Freestyle"
+    # ne lasa sa alegem ce tip de postare o sa fie si cu ajutorul ei o vom putea filtra
+    post_type = models.CharField(choices=PostType.choices,max_length=5)
+
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+
+
+    
