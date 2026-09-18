24f985b3f1fba32567b0da037e74a38bef79f3b4
bivol <vbivol394@gmail.com>
2026-09-18 09:46:08 +0300

    Lucrat la model Post

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -8,6 +10,7 @@ class Post(models.Model):
         SOUND = "Music", "Sound"
         IMAGE = "Gif", "Gifs"
         FREE = "Free", "Freestyle"
+
     # ne lasa sa alegem ce tip de postare o sa fie si cu ajutorul ei o vom putea filtra
     post_type = models.CharField(choices=PostType.choices,max_length=5)
 
@@ -15,7 +18,9 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
+    unique_views = models.IntegerField(default=0)
 
-
-    
+    def __str__(self):
+        return self.title
